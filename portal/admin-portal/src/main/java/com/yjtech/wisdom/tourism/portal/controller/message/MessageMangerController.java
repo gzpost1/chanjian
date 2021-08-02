88fe57545480e8f686ec29aae3592d8e48367162
renguangqian <rgq199551@123>
2021-08-02 10:13:43 +0800

    feat(标准产监/消息中心): 后台：新增最新消息条数接口

--- portal/admin-portal/src/main/java/com/yjtech/wisdom/tourism/portal/controller/message/MessageMangerController.java
+++ portal/admin-portal/src/main/java/com/yjtech/wisdom/tourism/portal/controller/message/MessageMangerController.java
@@ -3,14 +3,12 @@ package com.yjtech.wisdom.tourism.portal.controller.message;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.yjtech.wisdom.tourism.common.core.domain.JsonResult;
 import com.yjtech.wisdom.tourism.message.admin.dto.MessageDto;
+import com.yjtech.wisdom.tourism.message.admin.dto.MessageRecordDto;
 import com.yjtech.wisdom.tourism.message.admin.service.MessageMangerService;
 import com.yjtech.wisdom.tourism.message.admin.vo.QueryMessageVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 消息中心
@@ -25,8 +23,24 @@ public class MessageMangerController {
     @Autowired
     private MessageMangerService messageMangerService;
 
+    /**
+     * 查询消息列表
+     *
+     * @param vo
+     * @return
+     */
     @PostMapping("queryPageMessage")
-    public JsonResult<IPage<MessageDto>> queryPageMessage(@RequestBody @Validated QueryMessageVo vo) {
+    public JsonResult<IPage<MessageDto>> queryPageMessage (@RequestBody @Validated QueryMessageVo vo) {
         return JsonResult.success(messageMangerService.queryPageMessage(vo, true));
+    }
+
+    /**
+     * 查询当前用户新增消息
+     *
+     * @return
+     */
+    @GetMapping("queryNewMessageNum")
+    public JsonResult<MessageRecordDto> queryNewMessageNum () {
+        return JsonResult.success(messageMangerService.queryNewMessageNum());
     }
 }
