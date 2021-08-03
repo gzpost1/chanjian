cd174f6e02802384760824f26f6de1e323a66628
zhangcong <549152166@qq.com>
2021-08-03 17:56:42 +0800

    景区app分页接口

---
+++ portal/app-portal/src/main/java/com/yjtech/wisdom/tourism/portal/controller/scenic/ScenicAppController.java
@@ -0,0 +1,36 @@
+package com.yjtech.wisdom.tourism.portal.controller.scenic;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.yjtech.wisdom.tourism.common.core.domain.JsonResult;
+import com.yjtech.wisdom.tourism.resource.scenic.entity.ScenicEntity;
+import com.yjtech.wisdom.tourism.resource.scenic.query.ScenicPageQuery;
+import com.yjtech.wisdom.tourism.resource.scenic.service.ScenicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 景区app
+ *
+ * @author zc
+ * @since 2021-08-03
+ */
+@RestController
+@RequestMapping("/scenic/app")
+public class ScenicAppController {
+
+    @Autowired
+    private ScenicService scenicService;
+
+    /**
+     * 分页查询
+     * @Param:  query
+     * @return:
+     */
+    @PostMapping("/queryForPage")
+    public JsonResult<IPage<ScenicEntity>> queryForPage(@RequestBody ScenicPageQuery query) {
+        return JsonResult.success(scenicService.queryForPage(query));
+    }
+}
---
+++ portal/app-portal/src/main/java/com/yjtech/wisdom/tourism/portal/controller/scenic/ScenicScreenController.java
@@ -0,0 +1,15 @@
+package com.yjtech.wisdom.tourism.portal.controller.scenic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 景区大屏
+ *
+ * @author zc
+ * @since 2021-08-03
+ */
+@RestController
+@RequestMapping("/scenic/screen")
+public class ScenicScreenController {
+}
