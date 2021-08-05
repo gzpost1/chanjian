ddde4e052e55fc72fc11999093990718ae0809ec
gaoxingpu <gaoxingpu@yunjingtech.cn>
2021-08-05 19:49:33 +0800

    feat(标准产监/旅游投诉)：新增大屏接口 1.查询旅游投诉量 2.查询旅游投诉类型分布 3.查询旅游投诉状态分布 4.查询旅游投诉类型Top排行 5.查询本年旅游投诉趋势 6.查询分页 7.根据id查询

--- business/business-command/src/main/java/com/yjtech/wisdom/tourism/command/mapper/travelcomplaint/TravelComplaintMapper.java
+++ business/business-command/src/main/java/com/yjtech/wisdom/tourism/command/mapper/travelcomplaint/TravelComplaintMapper.java
@@ -7,7 +7,13 @@ import com.yjtech.wisdom.tourism.command.dto.travelcomplaint.TravelComplaintList
 import com.yjtech.wisdom.tourism.command.dto.travelcomplaint.TravelComplaintStatusStatisticsDTO;
 import com.yjtech.wisdom.tourism.command.entity.travelcomplaint.TravelComplaintEntity;
 import com.yjtech.wisdom.tourism.command.vo.travelcomplaint.TravelComplaintQueryVO;
+import com.yjtech.wisdom.tourism.command.vo.travelcomplaint.TravelComplaintScreenQueryVO;
+import com.yjtech.wisdom.tourism.common.bean.AnalysisMonthChartInfo;
+import com.yjtech.wisdom.tourism.common.bean.BasePercentVO;
+import com.yjtech.wisdom.tourism.common.bean.BaseVO;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 旅游投诉(TbTravelComplaint)表数据库访问层
@@ -39,6 +45,41 @@ public interface TravelComplaintMapper extends BaseMapper<TravelComplaintEntity>
      * @return
      */
     TravelComplaintStatusStatisticsDTO queryStatusStatistics(@Param("params") TravelComplaintQueryVO params);
+
+    /**
+     * 查询旅游投诉类型分布
+     * @param params
+     * @return
+     */
+    List<BasePercentVO> queryComplaintTypeDistribution(@Param("params") TravelComplaintScreenQueryVO params);
+
+    /**
+     * 查询旅游投诉状态分布
+     * @param params
+     * @return
+     */
+    List<BasePercentVO> queryComplaintStatusDistribution(@Param("params") TravelComplaintScreenQueryVO params);
+
+    /**
+     * 查询旅游投诉类型Top排行
+     * @param params
+     * @return
+     */
+    List<BaseVO> queryComplaintTopByType(@Param("params") TravelComplaintScreenQueryVO params);
+
+    /**
+     * 查询旅游投诉今年搜索月趋势
+     * @param params
+     * @return
+     */
+    List<AnalysisMonthChartInfo> queryComplaintCurrentAnalysisMonthInfo(@Param("params") TravelComplaintScreenQueryVO params);
+
+    /**
+     * 查询旅游投诉去年搜索月趋势
+     * @param params
+     * @return
+     */
+    List<AnalysisMonthChartInfo> queryComplaintLastAnalysisMonthInfo(@Param("params") TravelComplaintScreenQueryVO params);
 
 
 
