f7d320fc62012bbc92280e84e242b3342f480e85
renguangqian <rgq199551@123>
2021-08-24 09:54:57 +0800

    feat(标准产监/决策辅助): 模拟数据

--- business/business-decision-support/src/main/java/com/yjtech/wisdom/tourism/decisionsupport/business/service/DecisionSupportMockService.java
+++ business/business-decision-support/src/main/java/com/yjtech/wisdom/tourism/decisionsupport/business/service/DecisionSupportMockService.java
@@ -1,9 +1,12 @@
 package com.yjtech.wisdom.tourism.decisionsupport.business.service;
 
-import com.yjtech.wisdom.tourism.decisionsupport.business.dto.DecisionMockDto;
+import com.alibaba.fastjson.JSONObject;
+import com.yjtech.wisdom.tourism.common.constant.SimulationConstants;
+import com.yjtech.wisdom.tourism.systemconfig.simulation.dto.decisionsupport.SimulationDecisionSupportDTO;
+import com.yjtech.wisdom.tourism.systemconfig.simulation.factory.SimulationFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * 模拟数据
@@ -12,15 +15,23 @@ import java.util.List;
  * @date 2021/8/23 16:21
  */
 @Service
-public class DecisionSupportMockService {
+public class DecisionSupportMockService implements SimulationFactory<SimulationDecisionSupportDTO> {
 
-    /**
-     * 列表查询
-     *
-     * @return
-     */
-    public List<DecisionMockDto> queryList() {
-        // 查询指标库
-        return null;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Override
+    public Object parse(String json) {
+        return JSONObject.parseObject(json, SimulationDecisionSupportDTO.class);
+    }
+
+    @Override
+    public String toJSONBytes(SimulationDecisionSupportDTO obj) {
+        return JSONObject.toJSONString(obj);
+    }
+
+    @Override
+    public void generateMockRedisData(SimulationDecisionSupportDTO obj) {
+        redisTemplate.opsForValue().set(getCacheKey(SimulationConstants.DECISION), obj);
     }
 }
