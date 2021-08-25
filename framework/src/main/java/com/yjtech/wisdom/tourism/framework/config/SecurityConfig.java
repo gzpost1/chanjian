e9cef8c46b6640fb03e56e515b7debe402858654
xulei <631830646@qq.com>
2021-08-25 16:10:24 +0800

    feat(接口权限): 还原

--- framework/src/main/java/com/yjtech/wisdom/tourism/framework/config/SecurityConfig.java
+++ framework/src/main/java/com/yjtech/wisdom/tourism/framework/config/SecurityConfig.java
@@ -91,8 +91,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 过滤请求
                 .authorizeRequests()
                 // 对于登录login 验证码captchaImage 允许匿名访问
-                .antMatchers("/**")
-                .anonymous()
                 .antMatchers("/login", "/captchaImage")
                 .anonymous()
                 .antMatchers(HttpMethod.GET, "/*.html", "/**/*.html", "/**/*.css", "/**/*.js")
