b81304aa36d60e313138c8156f37b97c319d2d76
xulei <631830646@qq.com>
2021-08-26 10:49:36 +0800

    feat(应急预案 类型管理): 新增 删除 修改使用一个方法

--- portal/admin-portal/src/main/java/com/yjtech/wisdom/tourism/portal/controller/command/EmergencyPlanTypeController.java
+++ portal/admin-portal/src/main/java/com/yjtech/wisdom/tourism/portal/controller/command/EmergencyPlanTypeController.java
@@ -1,22 +1,15 @@
 package com.yjtech.wisdom.tourism.portal.controller.command;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yjtech.wisdom.tourism.command.dto.plan.EmergencyPlanTypeUpdateDto;
-import com.yjtech.wisdom.tourism.command.entity.plan.EmergencyPlanEntity;
 import com.yjtech.wisdom.tourism.command.entity.plan.EmergencyPlanTypeEntity;
 import com.yjtech.wisdom.tourism.command.query.plan.EmergencyPlanTypeQuery;
 import com.yjtech.wisdom.tourism.command.service.plan.EmergencyPlanService;
 import com.yjtech.wisdom.tourism.command.service.plan.EmergencyPlanTypeService;
-import com.yjtech.wisdom.tourism.common.constant.EntityConstants;
 import com.yjtech.wisdom.tourism.common.core.domain.JsonResult;
-import com.yjtech.wisdom.tourism.common.utils.AssertUtil;
-import com.yjtech.wisdom.tourism.common.utils.DeleteParam;
 import com.yjtech.wisdom.tourism.common.utils.ValidList;
-import com.yjtech.wisdom.tourism.common.utils.bean.BeanMapper;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -78,28 +71,7 @@ public class EmergencyPlanTypeController {
      */
     @PostMapping("/saveOrUpdate")
     public JsonResult create(@RequestBody @Valid ValidList<EmergencyPlanTypeUpdateDto> dtos) {
-        AssertUtil.isFalse(CollectionUtils.isEmpty(dtos), "不能为空");
-        List<EmergencyPlanTypeEntity> emergencyPlanTypeEntities = BeanMapper.mapList(dtos, EmergencyPlanTypeEntity.class);
-        emergencyPlanTypeService.saveOrUpdateBatch(emergencyPlanTypeEntities);
-        return JsonResult.ok();
-    }
-
-
-    /**
-     * 删除
-     *
-     * @param deleteParam
-     * @return
-     */
-    @PostMapping("/delete")
-    public JsonResult delete(@RequestBody @Valid DeleteParam deleteParam) {
-        //检查是否被引用
-        int count = emergencyPlanService.count(new LambdaQueryWrapper<EmergencyPlanEntity>().eq(EmergencyPlanEntity::getType, deleteParam.getId()));
-        AssertUtil.isFalse(count >= 1, "该类型被引用不能被删除");
-        LambdaUpdateWrapper<EmergencyPlanTypeEntity> updateWrapper = new LambdaUpdateWrapper<>();
-        updateWrapper.set(EmergencyPlanTypeEntity::getDeleted, EntityConstants.DELETED);
-        updateWrapper.eq(EmergencyPlanTypeEntity::getId, deleteParam.getId());
-        emergencyPlanTypeService.update(new EmergencyPlanTypeEntity(), updateWrapper);
+        emergencyPlanTypeService.create(dtos);
         return JsonResult.ok();
     }
 
