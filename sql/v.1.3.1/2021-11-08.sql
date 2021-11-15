0a99a7ffbd71def8534a5cdb8b5e1007cda99121
mujun <349573795@qq.com>
2021-11-15 15:26:19 +0800

    feat: 新增h5根节点

--- sql/v.1.3.1/2021-11-08.sql
+++ sql/v.1.3.1/2021-11-08.sql
@@ -12,3 +12,6 @@ ALTER TABLE tb_systemconfig_h5_menu ALTER COLUMN is_simulation SET DEFAULT 0;
 
 ALTER TABLE tb_systemconfig_h5_menu MODIFY point_data json COMMENT 'point_data 点位展示数据';
 ALTER TABLE tb_systemconfig_h5_menu MODIFY chart_data json COMMENT 'chart_data 图表数据';
+
+
+INSERT INTO tb_systemconfig_architecture (menu_id, menu_name, parent_id, first_id, secon_id, three_id, page_id, is_show, is_simulation, sort_num, create_user, create_time, update_user, update_time, deleted, type) VALUES (643455252847034384, '首页', 1, 643455252847034384, null, null, null, 1, 0, 1, 1, '2021-11-11 14:21:27', -1, null, 0, 1);
